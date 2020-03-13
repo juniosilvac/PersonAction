6435ccbaf9481028432232a5f9ee80082da69e7c
Junio Silva <junio.silvac@gmail.com>
2020-03-13 02:25:12 -0300

    configurando connection string

---
+++ PersonAction/PersonAction/Model/Context/MySqlContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonAction.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext()
+        {
+        }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
